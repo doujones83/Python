f40804c4bdfa25970c78f1b39503a2bacda973af
Dou <williamboswell1983@gmail.com>
2025-08-17 22:43:45 -0400

    Added a few files

---
+++ Projects/Division.py
@@ -0,0 +1,5 @@
+weight = 12341111
+height = 62
+
+bmi = weight / height ** 3
+(print(int(bmi)))
---
+++ Projects/Score.py
@@ -0,0 +1,5 @@
+score = 12
+vertical_leap = 3.0
+is_winning = True
+
+print(f"your score is = {score}, your vertical leap is {vertical_leap} You are winning the game {is_winning}")
