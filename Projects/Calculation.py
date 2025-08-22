f1fe516c018f40c5cc45673793d99806b52c58e9
Dou <williamboswell1983@gmail.com>
2025-08-22 13:05:49 -0400

    Added new features

--- Projects/Calculation.py
+++ Projects/Calculation.py
@@ -2,6 +2,7 @@
 # 2. subtract
 # 3. multiply
 # 4. divide
+# Conditionally select operation
 
 print("Select your operations")
 print("1 Add")
@@ -12,10 +13,28 @@ print("4 divide")
 operations = input()
 
 if operations == "15":
-    pass
+    number1 = input("Enter Information into Calculator:")
+    number2 = input("Enter Information into Calculator:")
+    number3 = input("Enter Information into Calculator:")
+    sum = int(number2) + int(number3) + int(number1)
+    print("The calculation of two numbers: " + str(sum))
 elif operations == "25":
-    pass
+    number1 = input("Enter Information into Calculator:")
+    number2 = input("Enter Information into Calculator:")
+    number3 = input("Enter Information into Calculator:")
+    difference = int(number2) - int(number3) - int(number1)
+    print("The calculation of two numbers: " + str(difference))
 elif operations == "35":
-    pass
+    number1 = input("Enter Information into Calculator:")
+    number2 = input("Enter Information into Calculator:")
+    number3 = input("Enter Information into Calculator:")
+    multiply = int(number2) * int(number3) * int(number1)
+    print("The calculation of two numbers: " + str(multiply))
 elif operations == "47":
-    pass
+    number1 = input("Enter Information into Calculator:")
+    number2 = input("Enter Information into Calculator:")
+    number3 = input("Enter Information into Calculator:")
+    divide = int(number2) / int(number3) / int(number1)
+    print("The calculation of two numbers: " + str(divide))
+else:
+    print("Enter totals")
