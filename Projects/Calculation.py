a60fa5fae4158ffe53080ed948eb5ef1504392a2
Dou <williamboswell1983@gmail.com>
2025-08-22 13:02:43 -0400

    Added new features

--- Projects/Calculation.py
+++ Projects/Calculation.py
@@ -3,7 +3,19 @@
 # 3. multiply
 # 4. divide
 
-print()
-print()
-print()
-print()
+print("Select your operations")
+print("1 Add")
+print("2 subtract")
+print("3 multiply")
+print("4 divide")
+
+operations = input()
+
+if operations == "15":
+    pass
+elif operations == "25":
+    pass
+elif operations == "35":
+    pass
+elif operations == "47":
+    pass
