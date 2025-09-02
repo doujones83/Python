e887c0494a994904c222a64f15b7ffd0c08b2ae3
Dou <williamboswell1983@gmail.com>
2025-09-02 11:49:55 -0400

    While Loops

---
+++ Projects/If-Else.py
@@ -0,0 +1,20 @@
+# if condition:
+# Do something if the condition is true
+# else:
+# Do something is the condition is false
+
+a = 27
+b = 104
+c = 111
+
+if a < b:
+    print("All conditions")
+
+numbers = 365
+
+if 365 < numbers:
+    print("The Statement")
+elif numbers == 360:
+    print("The numbers")
+else:
+    print("Numbers")
